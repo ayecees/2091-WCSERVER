dba3f12d912ad3ad13010872a3da3226a1fb1e5c
ayecees <122766890+ayecees@users.noreply.github.com>
2023-01-23 11:45:36 +0800

    Add files via upload
    Prelims initial files.

---
+++ prelims/app.js
@@ -0,0 +1,3 @@
+var myLogModule = require("./utility/log.js");
+
+myLogModule.warning("Warning node not found..");
---
+++ prelims/node-example.js
@@ -0,0 +1,13 @@
+//Name: Allen Carlo S. Serrano
+//Module: 3
+//Week 2 and 3: Basic of NodeJS
+
+//first prgoram
+console.log("Welcome to WCSERVER!")
+
+//Creating my first function
+function Display(x) {
+    console.log(x);
+}
+
+Display(200);
