4f23c396926059836a78e145a650ad6b3f1d59dc
ayecees <122766890+ayecees@users.noreply.github.com>
2023-01-23 21:30:48 +0800

    Add files via upload

---
+++ prelims/Lab-Activity-2/addNumbers/add.js
@@ -0,0 +1,8 @@
+//Allen Carlo S. Serrano
+function add(num1, num2){
+    let sum = num1 + num2;
+    
+    return sum;
+}
+
+module.exports = add;
---
+++ prelims/Lab-Activity-2/addNumbers/app.js
@@ -0,0 +1,4 @@
+//Allen Carlo S. Serrano
+const add = require ('./add.js');
+
+console.log("Sum: " + add(10,20));
