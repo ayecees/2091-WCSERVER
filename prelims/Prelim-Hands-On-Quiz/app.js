03ce522977ac5bfb43e5c7cf8604740614811822
ayecees <122766890+ayecees@users.noreply.github.com>
2023-01-30 21:39:27 +0800

    2 files added

---
+++ prelims/Prelim-Hands-On-Quiz/app.js
@@ -0,0 +1,24 @@
+var { add, subtract, multiply, divide } = require('./calculation.js');
+
+var RPH = 300;
+var HRS = 4;
+var days = 6;
+var pagibig = 300;
+var philhealth = 400;
+
+var HRSdays = multiply(HRS, days);
+var GIncome = multiply(RPH, HRSdays);
+var SSS = multiply(RPH, HRS);
+var Tax = divide(GIncome, 10);
+var deduction1 = add(Tax, SSS);
+var deduction2 = add(pagibig, philhealth);
+var Tdeduction = add(deduction1, deduction2);
+var nsalary = subtract(GIncome, Tdeduction);
+
+console.log('The gross income is ' + GIncome);
+console.log('Tax: ' + Tax);
+console.log('SSS: ' + SSS);
+console.log('Pag-Ibig fund: ' + pagibig);
+console.log('PhiilHealth: ' + philhealth);
+console.log('Total deductions: ' + Tdeduction);
+console.log('The net salary is: ' + nsalary);
---
+++ prelims/Prelim-Hands-On-Quiz/calculation.js
@@ -0,0 +1,20 @@
+//Allen Carlo S. Serrano
+//WD - 201
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+module.exports = { add, subtract, multiply, divide };
