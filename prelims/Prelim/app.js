373d2448b6dae45c284020b5fdf9f3c3a7ce370b
ayecees <122766890+ayecees@users.noreply.github.com>
2023-02-08 15:59:56 +0800

    2 files added

---
+++ prelims/Prelim/app.js
@@ -0,0 +1,10 @@
+//Allen Serrano
+//WD - 201
+
+var { greet, congratulations, farewell } = require('./function.js');
+
+var name = 'Allen Serrano';
+
+console.log(greet(name));
+console.log(congratulations(name));
+console.log(farewell(name));
---
+++ prelims/Prelim/function.js
@@ -0,0 +1,17 @@
+//Allen Serrano
+//WD - 201
+
+
+function greet(name) {
+  return 'Hello ' + name + '!';
+}
+
+function congratulations(name) {
+  return 'Congratulations, ' + name + '!';
+}
+
+function farewell(name) {
+  return 'Farewell ' + name + '!';
+}
+
+module.exports = { greet, congratulations, farewell };
