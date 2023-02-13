24b438837c58d53d6a0af7098c463e4e3b40b057
ayecees <122766890+ayecees@users.noreply.github.com>
2023-02-13 13:11:57 +0800

    1 file modified

--- midterms/secondapp.js
+++ midterms/secondapp.js
@@ -1,14 +1,13 @@
-var express = require("Express");
+var express = require('express');
 var app = express();
 
 const port = 2001;
 const host = 'localhost';
 
-app.get("/",function(req,res){
-    res.send("You have successfully created your second app!")
+app.get('/', function (request, response) {
+  response.send('You have successfully created your second app!');
 });
 
 app.listen(port, host, () => {
-  console.log('Example app listening at http://ks:ks')
-  host, port);
+  console.log(`Example app listening at http://%s:%s`, host, port);
 });
